9252c0122c3f20696e1c2ebe9943c0a238aa97e9
tgentry300 <taylor.gentry300@gmail.com>
2018-12-16 21:39:01 -0500

    fixed some flake8 errors

--- web-scraper.py
+++ web-scraper.py
@@ -18,20 +18,25 @@ def request_url(url):
     r = requests.get(url)
     html_text = r.content
 
-    url_list = list(set(re.findall(
-        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html_text)))
+    url_regex = (r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|'
+                 r'(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    url_list = list(set(re.findall(url_regex, html_text)))
 
     email_list = list(
-        set(re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', html_text)))
+        set(re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)',
+            html_text)))
 
     parser.feed(html_text)
     phone_list = []
     for d in parser.data:
-        phone_regex = r'\W*\D([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?\D'
+        phone_regex = (r'\W*\D([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*'
+                       r'([0-9]{4})(\se?x?t?(\d*))?\D')
         matches = re.search(phone_regex, d)
         if matches:
             phone_list.append(
-                '({}){}-{}'.format(matches.group(1), matches.group(2), matches.group(3)))
+                '({}){}-{}'.format(matches.group(1), matches.group(2),
+                                   matches.group(3)))
 
     print('URLs')
     pprint(url_list)
